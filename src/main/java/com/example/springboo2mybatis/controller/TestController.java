c52456e8750e950886fb9e337d81642c22a75f22
ethan <71875543@qq.com>
2018-12-11 20:49:35 +0800

    加入mybatis pagehelper

--- src/main/java/com/example/springboo2mybatis/controller/TestController.java
+++ src/main/java/com/example/springboo2mybatis/controller/TestController.java
@@ -1,19 +1,29 @@
 package com.example.springboo2mybatis.controller;
 
 
+import com.example.springboo2mybatis.beans.User;
+import com.example.springboo2mybatis.service.UserService;
+import com.github.pagehelper.PageInfo;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @Slf4j
 @RestController
 public class TestController {
 
+    @Autowired
+    private UserService userService;
 
-    @GetMapping(value = "test/{test}")
-    public void test(@PathVariable String test) {
-        log.info("test-----",test);
+
+    @GetMapping(value = "test/{page}/{size}")
+    public List<User> test(@PathVariable int page, @PathVariable int size) {
+        log.info("page-----", page);
+        return userService.queryAllUser(page, size);
     }
 }
