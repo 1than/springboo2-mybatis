83ea05887b6e12e8429a9da38103ce723a6cf088
ethan <71875543@qq.com>
2018-12-11 20:49:12 +0800

    加入mybatis pagehelper

--- src/main/java/com/example/springboo2mybatis/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/springboo2mybatis/service/impl/UserServiceImpl.java
@@ -3,11 +3,16 @@ package com.example.springboo2mybatis.service.impl;
 import com.example.springboo2mybatis.beans.User;
 import com.example.springboo2mybatis.dao.UserMapper;
 import com.example.springboo2mybatis.service.UserService;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+
+@Slf4j
 @Service
 public class UserServiceImpl implements UserService {
 
@@ -16,6 +21,10 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public List<User> queryAllUser(Integer page, Integer size) {
-        return null;
+        //PageHelper.startPage(page,size) 下面紧跟查询
+        PageHelper.startPage(page,size);
+        List<User> userList = userMapper.queryAllUser(page, size);
+        //PageInfo<User> pageInfo = new PageInfo<>();
+        return userList;
     }
 }
