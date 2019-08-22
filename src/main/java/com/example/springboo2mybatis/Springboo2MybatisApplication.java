3cd28eae670e5ab32423f81069c3b7b79a240a38
ethan <71875543@qq.com>
2019-08-23 00:42:23 +0800

    springboot 整合 mybatis plus

--- src/main/java/com/example/springboo2mybatis/Springboo2MybatisApplication.java
+++ src/main/java/com/example/springboo2mybatis/Springboo2MybatisApplication.java
@@ -3,11 +3,9 @@ package com.example.springboo2mybatis;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @MapperScan(value = "com.example.springboo2mybatis.dao")
-@EnableCaching
 public class Springboo2MybatisApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/example/springboo2mybatis/dao/StockInfoDAO.java
@@ -0,0 +1,9 @@
+package com.example.springboo2mybatis.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.springboo2mybatis.beans.StockInfo;
+
+
+public interface StockInfoDAO extends BaseMapper<StockInfo> {
+
+}
