d14f218f7eec4c10cfd8d8717131f69e438d4a62
ethan <71875543@qq.com>
2019-09-22 11:31:32 +0800

    增加 mybatis-plus 代码生成器

--- src/main/java/com/example/springboot2mybatis/Springboo2MybatisApplication.java
+++ src/main/java/com/example/springboot2mybatis/Springboo2MybatisApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan(value = "com.example.springboo2mybatis.dao")
+@MapperScan(value = "com.example.springboot2mybatis.dao")
 public class Springboo2MybatisApplication {
 
     public static void main(String[] args) {
